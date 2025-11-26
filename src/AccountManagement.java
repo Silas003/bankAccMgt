37ff6cd80bae65d7316082623c16b20e2111c6b3
Silas Kumi <silas.kumi@amalitech.com>
2025-11-26 16:40:47 +0000

    working for the view accounts feature

--- src/AccountManagement.java
+++ src/AccountManagement.java
@@ -1,40 +1,43 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+
 public class AccountManagement {
-}
 
-class AccountManager{
-    private ArrayList<Accounts.Account> accounts = new ArrayList<>(50);
-    private int accountCount;
+//    public class AccountManager{
+        private ArrayList<Accounts.Account> accounts = new ArrayList<>(50);
+        private int accountCount;
 
-    public void addAccount(Accounts.Account account){
-        this.accounts.add(account);
-    }
+        public void addAccount(Accounts.Account account){
+            this.accounts.add(account);
+        }
 
-    public Accounts.Account findAccount(String accountNumber){
+        public Accounts.Account findAccount(String accountNumber){
 
-        for(Accounts.Account account: accounts){
-            if(account.getAccountNUmber() == accountNumber){
-                return account;
+            for(Accounts.Account account: accounts){
+                if(account.getAccountNUmber() == accountNumber){
+                    return account;
+                }
             }
-        }
-        return null;
-    }
-
-    public ArrayList<Accounts.Account> viewAllAccounts(){
-        return this.accounts;
-    }
-
-    public double getTotalBalance(){
-        double totalBalance = 0;
-
-        for ( Accounts.Account account : accounts){
-            totalBalance += account.getBalance();
+            return null;
         }
 
-        return  totalBalance;
-    }
+        public ArrayList<Accounts.Account> viewAllAccounts(){
+            return Arrays.asList(this.accounts);
+        }
 
-    public int getAccountCount(){
-        return this.accounts.size();
-    }
+        public double getTotalBalance(){
+            double totalBalance = 0;
+
+            for ( Accounts.Account account : accounts){
+                totalBalance += account.getBalance();
+            }
+
+            return  totalBalance;
+        }
+
+        public int getAccountCount(){
+            return this.accounts.size();
+        }
+//    }
 }
+
